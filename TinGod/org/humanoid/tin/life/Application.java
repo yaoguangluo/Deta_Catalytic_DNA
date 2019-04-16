c71c0c484cd3b68930b2e40984c61954b8b13e41
yaoguangluo <fengyue1985>
2019-04-17 03:24:51 +0800

    Register the family and race conditions of Mr.Tin

--- TinGod/org/humanoid/tin/life/Application.java
+++ TinGod/org/humanoid/tin/life/Application.java
@@ -1,5 +1,25 @@
 package org.humanoid.tin.life;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import DNAProcessor.Family;
+import DNAProcessor.Life;
+import DNAProcessor.Race;
+
 public class Application{
 	public static void main(String[] argv) {
+		Life life= new Life();
+		life.born();
+		//register family
+		Family family= new Family();
+		ConcurrentHashMap<String, Life> familyLife= new ConcurrentHashMap<>();
+		familyLife.put("Tin", life);
+		family.setFamily(familyLife);
+		//register race
+		Race race= new Race();
+		ConcurrentHashMap<String, Family> raceFamily= new ConcurrentHashMap<>();
+		raceFamily.put("Royal", family);
+		race.setRace(raceFamily);
+		//loop the Hello World.
 	}
 }
