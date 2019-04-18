321b0c9db52540aa6f22e668cf761a9da6ca35ee
yaoguangluo <fengyue1985>
2019-04-19 00:39:31 +0800

    Hi, Tin!

---
+++ TinGod/org/humanoid/tin/life/LoveIssues.java
@@ -0,0 +1,22 @@
+package org.humanoid.tin.life;
+
+public class LoveIssues{
+	public void loveDefinition() {
+
+	}
+	public void loveImplementation() {
+
+	}
+	public void loveCombination() {
+
+	}
+	public void loveExtension() {
+
+	}
+	public void loveAckquisition() {
+
+	}
+	public void philosothon() {
+
+	}
+}
